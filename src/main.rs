6e40dcca71571091377a3f8a253810198b7be6e9
oliver <oliver.makins@protonmail.com>
2023-06-11 18:32:36 +0300

    updating main too

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 // use rust_light as rlt;
 use rust_light::nn::linear::Linear;
 use rust_light::optim::sgd::sgd_step;
-use rust_light::tensor::{Numeric, RcTensor, TensorLike};
+use rust_light::tensor::{RcTensor, TensorLike};
 
 fn main() {
     let mut layer = Linear::new(
         RcTensor::from([[1.0, -2.0], [-1.1, 0.7]]),
-        RcTensor::new_with_filler(vec![1, 2, 1], 1.0),
+        RcTensor::new_with_filler(vec![1, 2], 1.0),
         None,
     );
-    let input = RcTensor::new(vec![1.0, 2.0], vec![2, 1]);
+    let input = RcTensor::new(vec![1.0, 2.0], vec![1, 2]);
     let res = layer.forward(input);
     res.sum().backward();
     layer.weights.grad();
